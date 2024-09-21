4334f65f6d077ec1e46f28cfa0018779fb80a4a8
Ahmad Fajar Shidik <fajarshidik78@gmail.com>
2024-09-22 06:47:41 +0700

    [FIXING] Fix Create Customer Response structure

--- controller/customer_controller.go
+++ controller/customer_controller.go
@@ -11,6 +11,16 @@ type CustomerController interface {
 	CreateCustomer(ctx *gin.Context)
 }
 
+type CustomerResponse struct {
+	Message string `json:"message"`
+	Data  struct {
+		Customer_id int `json:"id"`
+		Name string `json:"name"`
+		Phone_number string `json:"phoneNumber"`
+		Address string `json:"address"`
+	} `json:"data"`
+}
+
 type customerController struct {
 	CustomerRepository repository.CustomerRepository
 }
@@ -33,6 +43,14 @@ func (cc *customerController) CreateCustomer(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message" : "Failed to create customer", "details" : err.Error()})
 		return
 	}
+	// Create The response struct 
+	response := CustomerResponse{
+		Message: "Successfuly Create Customer",
+	}
+	response.Data.Customer_id = createdCustomer.Customer_id
+	response.Data.Name = createdCustomer.Name
+	response.Data.Phone_number = createdCustomer.Phone_number
+	response.Data.Address = createdCustomer.Address
 
-	ctx.JSON(http.StatusCreated, gin.H{"data" : createdCustomer})
+	ctx.JSON(http.StatusCreated, response)
 }
--- repository/customer_repository.go
+++ repository/customer_repository.go
@@ -2,8 +2,8 @@ package repository
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
 	"submission-project-enigma-laundry/entity"
+	_ "github.com/lib/pq"
 )
 
 type CustomerRepository interface {
@@ -18,13 +18,13 @@ func NewCustomerRepo(db *sql.DB) CustomerRepository {
 	return &customerRepository{DB: db}
 }
 
-func (cr *customerRepository) CreateCustomer(customer *entity.Customer) (*entity.Customer,error) {
+func (cr *customerRepository) CreateCustomer(customer *entity.Customer) (*entity.Customer, error) {
 	// insert customer data into db
 	insert_query := "INSERT INTO customer (name,phone_number,address) VALUES ($1, $2, $3) RETURNING customer_id;"
 
-	err := cr.DB.QueryRow(insert_query, customer.Name, customer.Phone_number,customer.Address).Scan(&customer.Customer_id)
+	err := cr.DB.QueryRow(insert_query, customer.Name, customer.Phone_number, customer.Address).Scan(&customer.Customer_id)
 	if err != nil {
-		return customer , err // Handle error if the query fails
-	} 
-	return customer , nil
+		return customer, err // Handle error if the query fails
+	}
+	return customer, nil
 }
